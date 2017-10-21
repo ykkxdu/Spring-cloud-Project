6832e96f8826934d47d60d89fec70fe4b852aa66
ykkxdu <ykk_xdu@163.com>
2017-10-21 11:07:52 +0800

    Initial commit

---
+++ src/main/java/com/sselab/ykk/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.sselab.ykk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//启动服务注册中心
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
